e2d2378ab02fc8c0fb46627e742ac25c2b8cc7d7
Igor Todorovski <itodorov@ca.ibm.com>
2025-05-14 22:03:38 -0400

    Initial patch

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/fishport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/fishport/actions/workflows/bump.yml)
+
+# fish
+
+Fish shell
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/fishport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fishport.git'), string(name: 'PORT_DESCRIPTION', value: 'Fish shell' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
